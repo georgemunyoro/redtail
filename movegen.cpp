5ee67cbdfdfdd9f3c6acd71f53c6cf42b1d3db4c
George Munyoro <george.guva@outlook.com>
2021-01-16 22:13:40 +0200

    refactor: remove unused commented code in movegen function styling: formatting

--- movegen.cpp
+++ movegen.cpp
@@ -1,226 +1,264 @@
-#include "board.h"
-#include "defs.h"
 #include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
 
+#include "board.h"
+#include "defs.h"
+
 using namespace std;
 
-std::unordered_map<int, std::vector<int>> PieceMovements = {
-	{WhiteKnight, {N+N+E, E+E+N, S+S+E, W+W+S, S+E+E, S+S+W, N+N+W, N+W+W }},
-	{WhiteQueen,  {N, W, N+W, S+W, S, E, E+S, N+E }},
-	{WhiteRook,   {N, E, S, W}},
-	{WhiteKing,   {N, W, N+W, S+W, S, E, E+S, N+E }},
-	{WhiteBishop, {N+E, E+S, S+W, W+N }},
-	{BlackKnight, {N+N+E, E+ E+N, S+S+E, W+W+S, S+E+E, S+S+W, N+N+W, N+W+W }},
-	{BlackQueen,  {N, W, N+W, S+W, S, E, E+S, N+E }},
-	{BlackRook,   {N, E, S, W}},
-	{BlackKing,   {N, W, N+W, S+W, S, E, E+S, N+E }},
-	{BlackBishop, {N+ E, E+S, S+W, W+N }}
-};
+MoveList Board::newMoveGen() {
+  MoveList moves;
+  setKingPositions();
 
-vector<Move> Board::PseudoCaptures()
-{
-    vector<Move> moves;
+  for (int pos = 0; pos < 128; ++pos) {
+    if ((pos & 0x88) != 0 || GetColor(squares[pos]) != turn) continue;
+    int piece = squares[pos];
 
-    for (int pos = 0; pos < 128; ++pos) {
-        int piece = squares[pos];
+    if (piece == WhitePawn) {
+      if (squares[pos - 16] == Empty) {
+	moves.moves[moves.count] = (pos << 20) | ((pos - 16) << 12) |
+				   (squares[pos] << 8) |
+				   (squares[pos - 16] << 4) | castling;
+	moves.count++;
 
-        if ((pos & 0x88) != 0 || piece == Empty)
-            continue;
-        if (GetColor(piece) != turn)
-            continue;
+	if (squares[pos - 32] == Empty && (pos >> 4) == 6) {
+	  moves.moves[moves.count] = (pos << 20) | ((pos - 32)) << 12 |
+				     (squares[pos] << 8) |
+				     (squares[pos - 32] << 4) | castling;
+	  moves.count++;
+	}
+      }
 
-        if (piece == WhitePawn || piece == BlackPawn) {
-            vector<int> possible;
-            int start_row = turn == White ? 6 : 1;
-            if (turn == White) {
-                possible = { pos + N + E, pos + N + W };
-            } else {
-                possible = { pos + S + E, pos + S + W };
-            }
+      if (GetColor(squares[pos - 15]) == Black && (pos - 15 & 0x88) == 0) {
+	moves.moves[moves.count] = (pos << 20) | ((pos - 15)) << 12 |
+				   (squares[pos] << 8) |
+				   (squares[pos - 15] << 4) | castling;
+	moves.count++;
+      }
 
-            for (auto dest : { possible[0], possible[1] }) {
-                if (GetColor(squares[dest]) != None) {
-                    if ((turn == White && GetColor(squares[dest]) == Black) || (turn == Black && GetColor(squares[dest]))) {
-                        moves.push_back({ pos, dest, squares[pos], squares[dest], "attack" });
-                    }
-                }
-            }
-        } else {
-            vector<int> MOVES;
-            if (piece == WhiteKnight || piece == BlackKnight) {
-                MOVES = { N + N + E, E + E + N, S + S + E, W + W + S,
-                    S + E + E, S + S + W, N + N + W, N + W + W };
-            } else if (piece == WhiteQueen || piece == BlackQueen)
-                MOVES = { N, W, N + W, S + W, S, E, E + S, N + E };
-            else if (piece == WhiteRook || piece == BlackRook)
-                MOVES = { N, E, S, W };
-            else if (piece == WhiteKing || piece == BlackKing)
-                MOVES = { N, W, N + W, S + W, S, E, E + S, N + E };
-            else if (piece == WhiteBishop || piece == BlackBishop)
-                MOVES = { N + E, E + S, S + W, W + N };
+      if (GetColor(squares[pos - 17]) == Black && (pos - 17 & 0x88) == 0) {
+	moves.moves[moves.count] = (pos << 20) | ((pos - 17)) << 12 |
+				   (squares[pos] << 8) |
+				   (squares[pos - 17] << 4) | castling;
+	moves.count++;
+      }
 
-            for (int possibleMove : MOVES) {
-                int index = pos;
-                for (;;) {
-                    index += possibleMove;
-                    if ((index & 0x88) != 0)
-                        break;
-                    if ((turn == White && GetColor(squares[index]) == White) || (turn == Black && GetColor(squares[index]) == Black))
-                        break;
-                    if (squares[index] != Empty) {
-                        moves.push_back(
-                            { pos, index, squares[pos], squares[index], "attack" });
-                        break;
-                    }
-                    if (piece == WhiteKing || piece == WhiteKnight || piece == BlackKnight || piece == BlackKing)
-                        break;
-                }
-            }
-            vector<int>().swap(MOVES);
-        }
+    } else if (piece == BlackPawn) {
+      if (squares[pos + 16] == Empty) {
+	moves.moves[moves.count] = (pos << 20) | ((pos + 16) << 12) |
+				   (squares[pos] << 8) |
+				   (squares[pos + 16] << 4) | castling;
+	moves.count++;
+
+	if (squares[pos + 32] == Empty && (pos >> 4) == 1) {
+	  moves.moves[moves.count] = (pos << 20) | ((pos + 32)) << 12 |
+				     (squares[pos] << 8) |
+				     (squares[pos + 32] << 4) | castling;
+	  moves.count++;
+	}
+      }
+
+      if (GetColor(squares[pos + 15]) == White && (pos + 15 & 0x88) == 0) {
+	moves.moves[moves.count] = (pos << 20) | ((pos + 15)) << 12 |
+				   (squares[pos] << 8) |
+				   (squares[pos + 15] << 4) | castling;
+	moves.count++;
+      }
+
+      if (GetColor(squares[pos + 17]) == White && (pos + 17 & 0x88) == 0) {
+	moves.moves[moves.count] = (pos << 20) | ((pos + 17)) << 12 |
+				   (squares[pos] << 8) |
+				   (squares[pos + 17] << 4) | castling;
+	moves.count++;
+      }
     }
-    return moves;
+
+    if (piece == WhiteKing) {
+      for (int i = 0; i < 8; ++i) {
+	int dest = KingDirections[i] + pos;
+	if ((dest & 0x88) == 0 && GetColor(squares[dest]) != White &&
+	    !isSquareAttacked(dest, Black)) {
+	  moves.moves[moves.count] = (pos << 20) | ((dest)) << 12 |
+				     (squares[pos] << 8) |
+				     (squares[dest] << 4) | castling;
+	  moves.count++;
+	}
+      }
+    } else if (piece == BlackKing) {
+      for (int i = 0; i < 8; ++i) {
+	int dest = KingDirections[i] + pos;
+	if ((dest & 0x88) == 0 && GetColor(squares[dest]) != Black &&
+	    !isSquareAttacked(dest, White)) {
+	  moves.moves[moves.count] = (pos << 20) | ((dest)) << 12 |
+				     (squares[pos] << 8) |
+				     (squares[dest] << 4) | castling;
+	  moves.count++;
+	}
+      }
+    }
+
+    if (piece == WhiteKnight) {
+      for (int i = 0; i < 8; ++i) {
+	int dest = KnightDirections[i] + pos;
+	if ((dest & 0x88) == 0 && GetColor(squares[dest]) != White) {
+	  moves.moves[moves.count] = (pos << 20) | ((dest)) << 12 |
+				     (squares[pos] << 8) |
+				     (squares[dest] << 4) | castling;
+	  moves.count++;
+	}
+      }
+    } else if (piece == BlackKnight) {
+      for (int i = 0; i < 8; ++i) {
+	int dest = KnightDirections[i] + pos;
+	if ((dest & 0x88) == 0 && GetColor(squares[dest]) != Black) {
+	  moves.moves[moves.count] = (pos << 20) | ((dest)) << 12 |
+				     (squares[pos] << 8) |
+				     (squares[dest] << 4) | castling;
+	  moves.count++;
+	}
+      }
+    }
+
+    if (piece == WhiteRook || piece == WhiteQueen) {
+      for (int i = 0; i < 4; ++i) {
+	int dir = RookDirections[i];
+	int index = dir + pos;
+
+	while ((index & 0x88) == 0 && GetColor(squares[index]) != White) {
+	  moves.moves[moves.count] = (pos << 20) | ((index) << 12) |
+				     (squares[pos] << 8) |
+				     (squares[index] << 4) | castling;
+	  moves.count++;
+
+	  if (GetColor(squares[index]) == Black) break;
+
+	  index += dir;
+	}
+      }
+    } else if (piece == BlackRook || piece == BlackQueen) {
+      for (int i = 0; i < 4; ++i) {
+	int dir = RookDirections[i];
+	int index = dir + pos;
+
+	while ((index & 0x88) == 0 && GetColor(squares[index]) != Black) {
+	  moves.moves[moves.count] = (pos << 20) | ((index) << 12) |
+				     (squares[pos] << 8) |
+				     (squares[index] << 4) | castling;
+	  moves.count++;
+
+	  if (GetColor(squares[index]) == White) break;
+
+	  index += dir;
+	}
+      }
+    }
+
+    if (piece == WhiteBishop || piece == WhiteQueen) {
+      for (int i = 0; i < 4; ++i) {
+	int dir = BishopDirections[i];
+	int index = dir + pos;
+
+	while ((index & 0x88) == 0 && GetColor(squares[index]) != White) {
+	  moves.moves[moves.count] = (pos << 20) | ((index) << 12) |
+				     (squares[pos] << 8) |
+				     (squares[index] << 4) | castling;
+	  moves.count++;
+
+	  if (GetColor(squares[index]) == Black) break;
+
+	  index += dir;
+	}
+      }
+    } else if (piece == BlackBishop || piece == BlackQueen) {
+      for (int i = 0; i < 4; ++i) {
+	int dir = BishopDirections[i];
+	int index = dir + pos;
+
+	while ((index & 0x88) == 0 && GetColor(squares[index]) != Black) {
+	  moves.moves[moves.count] = (pos << 20) | ((index) << 12) |
+				     (squares[pos] << 8) |
+				     (squares[index] << 4) | castling;
+	  moves.count++;
+
+	  if (GetColor(squares[index]) == White) break;
+
+	  index += dir;
+	}
+      }
+    }
+  }
+
+  return moves;
 }
 
-vector<Move> Board::PseudoMoves()
-{
-    vector<Move> moves;
-    moves.reserve(256);
+bool Board::isSquareAttacked(int pos, int color) {
+  if (color == Black) {
+    if (((pos - 17) & 0x88) == 0 && squares[pos - 17] == BlackPawn) return true;
+    if (((pos - 15) & 0x88) == 0 && squares[pos - 15] == BlackPawn) return true;
+  } else if (color == White) {
+    if (((pos + 17) & 0x88) == 0 && squares[pos + 17] == WhitePawn) return true;
+    if (((pos + 15) & 0x88) == 0 && squares[pos + 15] == WhitePawn) return true;
+  }
 
-    for (int pos = 0; pos < 128; ++pos) {
-        int piece = squares[pos];
-
-        if ((pos & 0x88) != 0 || piece == Empty)
-            continue;
-        if (GetColor(piece) != turn)
-            continue;
-
-        if (piece == WhitePawn || piece == BlackPawn) {
-            vector<int> possible;
-            int start_row = turn == White ? 6 : 1;
-            if (turn == White) {
-                possible = { pos + N, pos + N + N, pos + N + E, pos + N + W };
-            } else {
-                possible = { pos + S, pos + S + S, pos + S + E, pos + S + W };
-            }
-
-            if (squares[possible[0]] == Empty) {
-                moves.push_back({ pos, possible[0], squares[pos], squares[possible[0]], "quiet" });
-                if (squares[possible[1]] == Empty && floor(pos / 16) == start_row) {
-                    Move move { pos, possible[0], squares[pos], squares[possible[0]], "quiet" };
-                    moves.push_back(
-                        { pos, possible[1], squares[pos], squares[possible[1]], "quiet" });
-                }
-            }
-
-            for (auto dest : { possible[2], possible[3] }) {
-                if (GetColor(squares[dest]) != None) {
-                    if (GetColor(squares[dest]) != turn && squares[dest] != 14) {
-                        moves.push_back({ pos, dest, squares[pos], squares[dest], "attack" });
-                    }
-                }
-            }
-        } else {
-            vector<int> MOVES;
-            if (piece == WhiteKnight || piece == BlackKnight) {
-                MOVES = { N + N + E, E + E + N, S + S + E, W + W + S,
-                    S + E + E, S + S + W, N + N + W, N + W + W };
-            } else if (piece == WhiteQueen || piece == BlackQueen)
-                MOVES = { N, W, N + W, S + W, S, E, E + S, N + E };
-            else if (piece == WhiteRook || piece == BlackRook)
-                MOVES = { N, E, S, W };
-            else if (piece == WhiteKing || piece == BlackKing)
-                MOVES = { N, W, N + W, S + W, S, E, E + S, N + E };
-            else if (piece == WhiteBishop || piece == BlackBishop)
-                MOVES = { N + E, E + S, S + W, W + N };
-
-            for (int possibleMove : MOVES) {
-                int index = pos;
-                for (;;) {
-                    index += possibleMove;
-                    if ((index & 0x88) != 0)
-                        break;
-                    if ((turn == White && GetColor(squares[index]) == White) || (turn == Black && GetColor(squares[index]) == Black))
-                        break;
-
-                    string move_type = squares[index] == Empty ? "quiet" : "attack";
-                    moves.push_back(
-                        { pos, index, squares[pos], squares[index], move_type });
-
-                    if (piece == WhiteKing || piece == WhiteKnight || piece == BlackKnight || piece == BlackKing)
-                        break;
-                    if (squares[index] != Empty)
-                        break;
-                }
-            }
-            vector<int>().swap(MOVES);
-        }
+  for (int i = 0; i < 8; ++i) {
+    int piece = squares[pos + KnightDirections[i]];
+    if (color == Black && piece == BlackKnight) {
+      return true;
+    } else if (color == White && piece == WhiteKnight) {
+      return true;
     }
-    return moves;
-}
+  }
 
-MoveList Board::generatePseudoMoves() {
-	MoveList moves;
+  for (int i = 0; i < 4; ++i) {
+    int dir = RookDirections[i];
+    int index = dir + pos;
+    int piece = squares[index];
 
-    for (int pos = 0; pos < 128; ++pos) {
-        int piece = squares[pos];
+    while ((index & 0x88) == 0) {
+      if (piece != Empty) {
+	if ((piece == WhiteRook || piece == BlackRook || piece == WhiteQueen ||
+	     piece == BlackQueen) &&
+	    GetColor(piece) == color)
+	  return true;
 
-        if ((pos & 0x88) != 0 || piece == Empty)
-            continue;
-        if (GetColor(piece) != turn)
-            continue;
-
-        if (piece == WhitePawn || piece == BlackPawn) {
-            vector<int> possible;
-            int start_row = turn == White ? 6 : 1;
-            if (turn == White) {
-                possible = { pos + N, pos + N + N, pos + N + E, pos + N + W };
-            } else {
-                possible = { pos + S, pos + S + S, pos + S + E, pos + S + W };
-            }
-
-            if (squares[possible[0]] == Empty) {
-                moves.moves[moves.count] = { pos, possible[0], squares[pos], squares[possible[0]], "quiet" };
-				moves.count++;
-                if (squares[possible[1]] == Empty && floor(pos / 16) == start_row) {
-					moves.moves[moves.count] = {pos, possible[1], squares[pos], squares[possible[1]], "quiet"};
-					moves.count++;
-                }
-            }
-
-            for (auto dest : { possible[2], possible[3] }) {
-                if (GetColor(squares[dest]) != None) {
-                    if (GetColor(squares[dest]) != turn && squares[dest] != 14) {
-						moves.moves[moves.count] = {pos, dest, squares[pos], squares[dest], "attack"};
-						moves.count++;
-                    }
-                }
-            }
-        } else {
-
-            for (int possibleMove : PieceMovements[piece]) {
-                int index = pos;
-                for (;;) {
-                    index += possibleMove;
-                    if ((index & 0x88) != 0) break;
-
-					if (GetColor(squares[index]) == turn)
-						break;
-
-					moves.moves[moves.count] = {pos, index, squares[pos], squares[index], squares[index] == Empty ? "quiet" : "attack"};
-					moves.count++;
-
-                    if (piece == WhiteKing || piece == WhiteKnight || piece == BlackKnight || piece == BlackKing)
-                        break;
-                    if (squares[index] != Empty)
-                        break;
-                }
-            }
-        }
+	break;
+      }
+      index += dir;
+      piece = squares[index];
     }
-	return moves;
+  }
+
+  for (int i = 0; i < 4; ++i) {
+    int dir = BishopDirections[i];
+    int index = dir + pos;
+    int piece = squares[index];
+
+    while ((index & 0x88) == 0) {
+      if (piece != Empty) {
+	if ((piece == WhiteBishop || piece == BlackBishop ||
+	     piece == WhiteQueen || piece == BlackQueen) &&
+	    GetColor(piece) == color)
+	  return true;
+
+	break;
+      }
+      index += dir;
+      piece = squares[index];
+    }
+  }
+
+  for (int i = 0; i < 8; ++i) {
+    if ((pos + KingDirections[i] & 0x88) != 0) continue;
+    int piece = squares[pos + KingDirections[i]];
+    if (color == White && piece == WhiteKing) {
+      return true;
+    } else if (color == Black && piece == BlackKing) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
